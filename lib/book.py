fa589d4b367586d35c2ba86022a8cb4fa48b8e57
vincenttommi <vincenttommi@gmail.com>
2023-05-18 14:12:03 +0300

    lab

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,22 @@
-#!/usr/bin/env python3
-
 class Book:
-    pass
-    
-        
+    def init(self, title, page_count):
+        self.title = title
+        self.set_page_count(page_count)
+
+    def get_title(self):
+        return self.title
+
+    def set_title(self, new_title):
+        self.title = new_title
+
+    def get_page_count(self):
+        return self.page_count
+
+    def set_page_count(self, new_page_count):
+        if isinstance(new_page_count, int):
+            self.page_count = new_page_count
+        else:
+            print("Page count must be an integer")
+
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,28 @@
-#!/usr/bin/env python3
-
 class Shoe:
-    pass
+    def init(self, brand, size):
+        self.brand = brand
+        self.set_size(size)
+        self.condition = "New"
+
+    def get_brand(self):
+        return self.brand
+
+    def set_brand(self, new_brand):
+        self.brand = new_brand
+
+    def get_size(self):
+        return self.size
+
+    def set_size(self, new_size):
+        if isinstance(new_size, int):
+            self.size = new_size
+        else:
+            raise ValueError("Size must be an integer")
+
+    def repair(self):
+        print("The shoe has been repaired.")
+        self.condition = "New"
+
+    def cobble(self):
+        print("Your shoe is as good as new!")
+        self.condition = "New"
